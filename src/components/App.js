6aadb105aff02ac4bb8a7b765815a9db893becd3
YaPopovAndrey <popov-1710@ya.ru>
2021-07-25 13:04:57 +0300

    Закончил пункт 4 ПР 10

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
+
+function App() {
+  return (
+      <body className="body">
+    <div className="root">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+</body>
+  );
+}
+
+export default App;
